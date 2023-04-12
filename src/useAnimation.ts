36d01652283208ba329de0f6c5fdedd06544a606
Firere <67512865+Firere@users.noreply.github.com>
2023-04-13 00:48:22 +0200

    Compatibility with @firere/roact

--- src/useAnimation.ts
+++ src/useAnimation.ts
@@ -1,91 +1,86 @@
-import Object from '@rbxts/object-utils'
-import Roact from '@rbxts/roact'
-import { useEffect, useState } from '@rbxts/roact-hooked'
-import { TweenService } from '@rbxts/services'
-import { AnimationTransition, AnimationVariants } from '.'
-
+import Roact from "@firere/roact";
+import { useEffect, useState } from "@firere/roact-hooked";
+import Object from "@rbxts/object-utils";
+import { TweenService } from "@rbxts/services";
+import { AnimationTransition, AnimationVariants } from ".";
 
 export default function useAnimation<T extends Instance>(
-    variants: AnimationVariants<T>,
-    ref: Roact.Ref<T>,
-    initial?:
-        | keyof AnimationVariants<T>
-        | (Partial<T> & {
-              transition?: Partial<AnimationTransition>
-          }),
-    animate?:
-        | keyof AnimationVariants<T>
-        | (Partial<T> & {
-              transition?: Partial<AnimationTransition>
-          }),
-    transition?: Partial<AnimationTransition>
+	variants: AnimationVariants<T>,
+	ref: Roact.Ref<T>,
+	initial?:
+		| keyof AnimationVariants<T>
+		| (Partial<T> & {
+				transition?: Partial<AnimationTransition>;
+		  }),
+	animate?:
+		| keyof AnimationVariants<T>
+		| (Partial<T> & {
+				transition?: Partial<AnimationTransition>;
+		  }),
+	transition?: Partial<AnimationTransition>,
 ): [string, (variant: string) => void] {
-    const [variantState, setVariantState] = useState<
-        | keyof AnimationVariants<T>
-        | (Partial<T> & {
-              transition?: Partial<AnimationTransition>
-          })
-    >();
-    const currentVariant = typeIs(variantState, 'string') ? variants[variantState] : variantState
-    const animationVariant =
-        animate !== undefined
-            ? typeIs(animate, 'string')
-                ? variants[animate]
-                : animate
-            : currentVariant
+	const [variantState, setVariantState] = useState<
+		| keyof AnimationVariants<T>
+		| (Partial<T> & {
+				transition?: Partial<AnimationTransition>;
+		  })
+	>();
+	const currentVariant = typeIs(variantState, "string") ? variants[variantState] : variantState;
+	const animationVariant =
+		animate !== undefined ? (typeIs(animate, "string") ? variants[animate] : animate) : currentVariant;
 
-    let animationProperties: Partial<T> = {}
-    let mergedTransition: Partial<AnimationTransition> = {}
+	let animationProperties: Partial<T> = {};
+	let mergedTransition: Partial<AnimationTransition> = {};
 
-    if(animationVariant) {
-        animationProperties = Object.entries(animationVariant)
-        .filter(([key]) => key !== 'transition')
-        .reduce((acc, [key, value]) => {
-            ;(acc as Record<string, unknown>)[key as string] = value
-            return acc
-        }, {} as Partial<T>)
+	if (animationVariant) {
+		animationProperties = Object.entries(animationVariant)
+			.filter(([key]) => key !== "transition")
+			.reduce((acc, [key, value]) => {
+				(acc as Record<string, unknown>)[key as string] = value;
+				return acc;
+			}, {} as Partial<T>);
 
-        mergedTransition = {
-            ...animationVariant.transition,
-            ...transition
-        }
-    }
-    useEffect(() => {
-        const element = ref.getValue()
-        if(!element) return
+		mergedTransition = {
+			...animationVariant.transition,
+			...transition,
+		};
+	}
+	useEffect(() => {
+		const element = ref.getValue();
+		if (!element) return;
 
-        Object.entries(initial ?? {}).forEach(([key, value]) => {
-            if(key !== 'transition') {
-                ;(element as Record<string, unknown>)[key as string] = value
-            }
-        })
+		Object.entries(initial ?? {}).forEach(([key, value]) => {
+			if (key !== "transition") {
+				(element as Record<string, unknown>)[key as string] = value;
+			}
+		});
 
-        let tween: Tween | undefined;
-        if(animationVariant) {
-            tween = TweenService.Create(
-                element,
-                new TweenInfo(
-                    mergedTransition.duration ?? 1,
-                    mergedTransition.easingStyle ?? Enum.EasingStyle.Linear,
-                    mergedTransition.easingDirection ?? Enum.EasingDirection.InOut,
-                    mergedTransition.repeatCount ?? 0,
-                    mergedTransition.reverses ?? false,
-                    mergedTransition.delay ?? 0
-                ),
-                animationProperties
-            )
-    
-            tween.Play()
-        }
+		let tween: Tween | undefined;
+		if (animationVariant) {
+			tween = TweenService.Create(
+				element,
+				new TweenInfo(
+					mergedTransition.duration ?? 1,
+					mergedTransition.easingStyle ?? Enum.EasingStyle.Linear,
+					mergedTransition.easingDirection ?? Enum.EasingDirection.InOut,
+					mergedTransition.repeatCount ?? 0,
+					mergedTransition.reverses ?? false,
+					mergedTransition.delay ?? 0,
+				),
+				animationProperties,
+			);
 
-        return () => {
-            tween?.Destroy()
-        }
-    }, [ref, variants, variantState, animate, transition])
+			tween.Play();
+		}
 
-    const setVariant = (variant: string) => {
-        setVariantState(variant)
-    }
+		return () => {
+			tween?.Destroy();
+		};
+	}, [ref, variants, variantState, animate, transition]);
 
-    return [typeIs(variantState, 'string') ? variantState : '', setVariant]
+	const setVariant = (variant: string) => {
+		setVariantState(variant);
+	};
+
+	return [typeIs(variantState, "string") ? variantState : "", setVariant];
 }
