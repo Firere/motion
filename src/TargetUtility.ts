25d417fd59232a654027fa9bde97b8d077358a15
Firere <67512865+Firere@users.noreply.github.com>
2024-09-02 02:35:13 +0100

    refactor: currying in OOP. sad!

--- src/TargetUtility.ts
+++ src/TargetUtility.ts
@@ -1,6 +1,8 @@
 import Object from "@rbxts/object-utils";
-import { AnimationProps, CastsToTarget, Transition } from ".";
+import type { AnimationProps, CastsToTarget, Target, Transition } from ".";
 import { Callback } from "./CustomTween/src";
+
+// I wanted to decompose a bit of `useTween` and ended up mixing currying and OOP. extremely cursed! but it works better imo
 
 export default class TargetUtil<T extends Instance> {
 	private defaultTransition?: Transition;
@@ -11,9 +13,7 @@ export default class TargetUtil<T extends Instance> {
 		this.variants = variants;
 	}
 
-	public addDefaultTransition(targetOrVariant: CastsToTarget<T>) {
-		const target = this.castToTarget(targetOrVariant);
-
+	public addDefaultTransition(target: Target<T>) {
 		if (this.defaultTransition === undefined) return target;
 		if (target.transition === undefined) return { ...target, transition: this.defaultTransition };
 
