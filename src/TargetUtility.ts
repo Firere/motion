3ec27d3d0913a32bba9da6b49a42471bf7eb8a41
Firere <67512865+Firere@users.noreply.github.com>
2024-09-02 03:27:37 +0100

    feat: move `castToTargets` into `TargetUtility`, decompose it and fully adopt `TargetUtility` in `useTween`

--- src/TargetUtility.ts
+++ src/TargetUtility.ts
@@ -1,5 +1,6 @@
 import Object from "@rbxts/object-utils";
-import type { AnimationProps, CastsToTarget, Target, Transition } from ".";
+import { t } from "@rbxts/t";
+import type { AnimationProps, CastsToTarget, CastsToTargets, Target, Transition } from ".";
 import { Callback } from "./CustomTween/src";
 
 // I wanted to decompose a bit of `useTween` and ended up mixing functional and OOP. extremely cursed! but it works better imo
@@ -44,5 +45,35 @@ export default class TargetUtil<T extends Instance> {
 			`Variant "${tostring(variant)}" is invalid: ${Object.keys(this.variants).join(", ")}`,
 		);
 		return this.variants[variant];
+	}
+
+	public castToTargets(targets?: CastsToTargets<T>, skipTransition?: boolean) {
+		if (targets === undefined) return undefined;
+
+		const mergeTransitions = (target: CastsToTarget<T>) =>
+			skipTransition ? this.castToTarget(target) : this.addDefaultTransition(this.castToTarget(target));
+
+		if (t.array(t.union(t.string, t.table))(targets)) return this.resolveConflicts(targets.map(mergeTransitions));
+
+		return [mergeTransitions(targets)];
+	}
+
+	public resolveConflicts(targets: Target<T>[]) {
+		const resolved = [...targets];
+
+		// some targets may contain the same property, resulting in multiple
+		// Tweens potentially messing with it, so all conflicts are handled here
+		const alreadyModified = new Set<keyof Partial<Extract<T, Tweenable>>>();
+		for (let i = resolved.size() - 1; i >= 0; i--)
+			for (const [key] of pairs(resolved[i] as object)) {
+				if (key === "transition") continue;
+				if (alreadyModified.has(key as never)) {
+					delete resolved[i][key as never];
+				} else {
+					alreadyModified.add(key as never);
+				}
+			}
+
+		return resolved;
 	}
 }
