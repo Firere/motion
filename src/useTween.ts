25efbe1e8d5d4a94473450ff7f21fce9a406903c
Firere <67512865+Firere@users.noreply.github.com>
2024-12-23 02:18:58 +0000

    DRY

--- src/useTween.ts
+++ src/useTween.ts
@@ -23,7 +23,7 @@ function tween<T extends Instance>(instance: T, targets: Target<T>[]) {
 				{
 					time: duration,
 					easing,
-					repeatCount: transition.repeat ?? repeatCount ?? 0,
+					repeatCount,
 					reverses,
 					delayTime: delay,
 					callback,
@@ -64,7 +64,7 @@ function tween<T extends Instance>(instance: T, targets: Target<T>[]) {
 				const [style, direction] = native;
 				const tween = TweenService.Create(
 					instance,
-					new TweenInfo(duration, style, direction, transition.repeat ?? repeatCount ?? 0, reverses, delay),
+					new TweenInfo(duration, style, direction, repeatCount, reverses, delay),
 					properties,
 				);
 				if (callback) tween.Completed.Connect(callback);
